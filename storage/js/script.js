5c91cba5c506024af39261705ed071ed5e063b52
Brunys <brunys@brunysixl.xyz>
2024-10-28 19:32:44 -0400

    Create script.js

---
+++ storage/js/script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    const loadingScreen = document.getElementById('loading-screen');
+    loadingScreen.style.opacity = '0';
+    setTimeout(() => {
+        loadingScreen.style.display = 'none';
+    }, 500);
+};
