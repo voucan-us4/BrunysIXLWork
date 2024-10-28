66989304edc10783580635391cc64fb9fee76458
Brunys <brunys@brunysixl.xyz>
2024-10-28 19:56:20 -0400

    Update box.js

--- storage/js/box.js
+++ storage/js/box.js
@@ -1,7 +1,16 @@
-window.onload = function() {
-    const loadingScreen = document.getElementById('loading-screen');
-    loadingScreen.style.opacity = '0';
-    setTimeout(() => {
-        loadingScreen.style.display = 'none';
-    }, 500);
-};
+function filterItems() {
+    const searchInput = document.getElementById('search').value.toLowerCase();
+    console.log("Search input:", searchInput); 
+    const items = document.querySelectorAll('.image-item');
+
+    items.forEach(item => {
+        const label = item.getAttribute('data-label').toLowerCase();
+        console.log("Checking item:", label); 
+        if (label.includes(searchInput)) {
+            item.style.display = ''; 
+        } else {
+            item.style.display = 'none'; 
+        }
+    });
+}
+
