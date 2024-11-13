d8c6f832e7570ae4705ef6a338dccc9345c4688f
Brunys <brunys@brunysixl.xyz>
2024-11-12 19:56:06 -0500

    Update box.js

--- storage/js/box.js
+++ storage/js/box.js
@@ -1,49 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const container = document.getElementById('imageContainer');
-    let lastClickedItems = JSON.parse(localStorage.getItem('lastClickedItems')) || [];
-
-    if (lastClickedItems.length) {
-        lastClickedItems.slice(10, 0).forEach(label => {
-            const itemToMove = document.querySelector(`.image-item[data-label="${label}"]`);
-            if (itemToMove) {
-                container.prepend(itemToMove);
-            }
+    function filterItems() {
+        const searchInput = document.getElementById('search').value.toLowerCase();
+        document.querySelectorAll('.image-item').forEach(item => {
+            const label = item.dataset.label.toLowerCase();
+            item.style.display = label.includes(searchInput) ? '' : 'none';
         });
     }
 
-    document.querySelectorAll('.image-item a').forEach(link => {
-        link.addEventListener('click', handleItemClick);
-    });
-
-    function handleItemClick(event) {
-        event.preventDefault();
-        const item = event.target.closest('.image-item');
-        const label = item.dataset.label;
-        const href = item.querySelector('a').getAttribute('href');
-
-        if (href) {
-            updateLastClickedItems(label);
-            window.location.href = href;
-        }
+    function moveToTop(event) {
+        const container = document.querySelector('.container');
+        const clickedImage = event.currentTarget;
+        container.prepend(clickedImage);
     }
 
-    function updateLastClickedItems(label) {
-        lastClickedItems = lastClickedItems.filter(existingLabel => existingLabel !== label);
-        lastClickedItems.unshift(label);
-
-        if (lastClickedItems.length > 10) {
-            lastClickedItems.pop();
-        }
-
-        localStorage.setItem('lastClickedItems', JSON.stringify(lastClickedItems));
-    }
-});
-
-function filterItems() {
-    const searchInput = document.getElementById('search').value.toLowerCase();
+    document.getElementById('search').addEventListener('input', filterItems);
 
     document.querySelectorAll('.image-item').forEach(item => {
-        const label = item.dataset.label.toLowerCase();
-        item.style.display = label.includes(searchInput) ? '' : 'none';
+        item.addEventListener('click', moveToTop);
     });
-}
+});
