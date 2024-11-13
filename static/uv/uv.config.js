f31a3840c3d87fdf9e6642a7a6036bcccd3bb6a8
Brunys <brunys@brunysixl.xyz>
2024-11-12 23:10:00 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
-    prefix: '/static/biw/',
-    bare:'https://bare.ubg.icu/bare/',
+    prefix: '/static/brunys/ixl/work/',
+    bare:'https://static.99.135.156.178.clients.your-server.de',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
